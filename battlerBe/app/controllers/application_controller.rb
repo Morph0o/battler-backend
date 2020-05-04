ecf590f08ee0775bb18f1da1e395c0115d12eff9
Tiffany Abraham <tony.calablaval@gmail.com>
2020-05-04 00:37:42 -0400

    Completed application_controller, users_controller, auth_controller. Implemented all with JWT and bcrypt.

--- battlerBe/app/controllers/application_controller.rb
+++ battlerBe/app/controllers/application_controller.rb
@@ -1,2 +1,50 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  before_action :allow_cross_domain_ajax
+  #before_action :require_login
+
+  def allow_cross_domain_ajax
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = 'POST, OPTIONS'
+  end
+
+
+
+
+  def encode_token(payload)
+    JWT.encode(payload, 'my_secret')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        []
+      end
+    end
+  end
+
+  def session_user
+    decoded_hash = decoded_token
+    if !decoded_hash.empty?
+      puts decoded_hash.class
+      user_id = decoded_hash[0]['user_id']
+      @user = User.find_by(id: user_id)
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!session_user
+  end
+
+  def require_login
+    render json: {message: 'Please Login'}, status: :unauthorized unless logged_in?
+  end
 end
--- battlerBe/app/controllers/users_controller.rb
+++ battlerBe/app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  #skip_before_action :require_login, only: [:create]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      #byebug
+      payload = {user_id: @user.id}
+      token = encode_token(payload)
+      puts token
+      render json: {user: @user, jwt: token}
+    else
+      render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:id, :name, :password, :desc)
+  end
 end
